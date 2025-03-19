3beec65e0172b7569f55099584d29e92fa076178
kwakseobang <iii6602@naver.com>
2025-03-19 23:13:08 +0900

    Setting[#1]: Swagger 연동 성공

---
+++ src/main/java/com/kwakmunsu/board/global/config/SwaggerConfig.java
@@ -0,0 +1,26 @@
+package com.kwakmunsu.board.global.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+            .components(new Components())
+            .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+            .title("Board - Swagger API")
+            .description("멋쟁이사자처럼 13기 스프링 스터디")
+            .version("1.0.0");
+    }
+
+}
