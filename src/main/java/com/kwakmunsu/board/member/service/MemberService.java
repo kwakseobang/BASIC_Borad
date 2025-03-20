857068323d3d5555fa76815cd720363debcce098
kwakseobang <iii6602@naver.com>
2025-03-21 00:54:44 +0900

    Feat[#7]: 로그아웃 기능 구현

--- src/main/java/com/kwakmunsu/board/member/service/MemberService.java
+++ src/main/java/com/kwakmunsu/board/member/service/MemberService.java
@@ -1,5 +1,7 @@
 package com.kwakmunsu.board.member.service;
 
+import com.kwakmunsu.board.member.entity.Member;
+import com.kwakmunsu.board.member.infrastruture.MemberReader;
 import com.kwakmunsu.board.member.infrastruture.MemberUpdater;
 import com.kwakmunsu.board.member.service.dto.NewNicknameDto;
 import lombok.RequiredArgsConstructor;
@@ -10,9 +12,15 @@ import org.springframework.stereotype.Service;
 public class MemberService {
 
     private final MemberUpdater memberUpdater;
+    private final MemberReader memberReader;
 
     public void updateNickname(Long memberId, NewNicknameDto newNicknameDto) {
         memberUpdater.updateNickname(memberId,newNicknameDto.nickname());
+    }
+
+    public void logout(Long memberId) {
+        Member member = memberReader.getMember(memberId);
+        member.updateRefreshToken(null); // 로그아웃 시 삭제해주기 위해 null 사용
     }
 
 }
--- src/main/java/com/kwakmunsu/board/util/CookieUtil.java
+++ src/main/java/com/kwakmunsu/board/util/CookieUtil.java
@@ -7,12 +7,19 @@ public final class CookieUtil {
     private CookieUtil() {
     }
 
-    public static Cookie createCookie(String key, String value) {
+    public static Cookie create(String key, String value) {
         Cookie cookie = new Cookie(key, value);
         cookie.setMaxAge(60 * 60 * 60);
         //cookie.setSecure(true);
         cookie.setPath("/");
         cookie.setHttpOnly(true);
+        return cookie;
+    }
+
+    public static Cookie delete(String key) {
+        Cookie cookie = new Cookie(key, null);
+        cookie.setMaxAge(0);
+        cookie.setPath("/");
         return cookie;
     }
 
