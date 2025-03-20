db93724fd23651a53a735cda2abd0251e09a2c38
kwakseobang <iii6602@naver.com>
2025-03-20 17:59:32 +0900

    File[#5]: 패키지명 수정

--- src/main/java/com/kwakmunsu/board/favoritesPost/repository/FavoritesPostJpaRepository.java
+++ src/main/java/com/kwakmunsu/board/favoritesPost/repository/FavoritesPostJpaRepository.java
@@ -1,4 +1,4 @@
-package com.kwakmunsu.board.favoritesPost.repository;
+package com.kwakmunsu.board.favoritespost.repository;
 
 import com.kwakmunsu.board.comment.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
