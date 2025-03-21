06c142ea4d50abc1a2a9ad8cb9f60a172a316c2f
kwakseobang <iii6602@naver.com>
2025-03-21 16:59:53 +0900

    Chore[#15]: PostCommand -> PostCreateCommand  복구

--- src/main/java/com/kwakmunsu/board/post/service/dto/request/PostCommand.java
+++
@@ -1,8 +0,0 @@
-package com.kwakmunsu.board.post.service.dto.request;
-
-import lombok.Builder;
-
-@Builder
-public record PostCommand(String title, String content, Long memberId) {
-
-}
---
+++ src/main/java/com/kwakmunsu/board/post/service/dto/request/PostCreateCommand.java
@@ -0,0 +1,12 @@
+package com.kwakmunsu.board.post.service.dto.request;
+
+import lombok.Builder;
+
+@Builder
+public record PostCreateCommand(
+        String title,
+        String content,
+        Long memberId
+) {
+
+}
