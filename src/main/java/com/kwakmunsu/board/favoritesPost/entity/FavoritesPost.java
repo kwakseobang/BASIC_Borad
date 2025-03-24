528d145d74925091ec19f75e09c32b24abdb9f18
kwakseobang <iii6602@naver.com>
2025-03-24 13:10:08 +0900

    Feat[#39]: 저장 방식 변경 member -> memberId

--- src/main/java/com/kwakmunsu/board/favoritesPost/entity/FavoritesPost.java
+++ src/main/java/com/kwakmunsu/board/favoritesPost/entity/FavoritesPost.java
@@ -1,21 +1,16 @@
 package com.kwakmunsu.board.favoritespost.entity;
 
 import com.kwakmunsu.board.global.entity.BaseEntity;
-import com.kwakmunsu.board.member.entity.Member;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.Fetch;
 
 
 @Table(name = "favorites_post")
@@ -32,14 +27,13 @@ public class FavoritesPost extends BaseEntity {
     @Column(name = "post_id", nullable = false)
     private Long postId;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "member_id", nullable = false)
-    private Member member;
+    @Column(name = "member_id", nullable = false)
+    private Long memberId;
 
     @Builder
-    public FavoritesPost(Long postId, Member member) {
+    public FavoritesPost(Long postId, Long memberId) {
         this.postId = postId;
-        this.member = member;
+        this.memberId = memberId;
     }
 
 }
